fcbdd378e2f4a44fedcded41e4fdc29f911cbddd
L-tech <legendabrahamonoja@gmail.com>
2022-02-05 23:52:25 +0100

    update: update testnet labelling

--- contracts/RandomNumber.sol
+++ contracts/RandomNumber.sol
@@ -13,7 +13,7 @@ contract RandomNumberConsumer is VRFConsumerBase {
     /**
      * Constructor inherits VRFConsumerBase
      * 
-     * Network: Kovan
+     * Network: Rinkeby
      * Chainlink VRF Coordinator address: 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
      * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
      * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
